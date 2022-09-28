7c0d824b1964ebaf25a7db813a875b9063ef8f09
ali <ali.kazemi1@brtech.ir>
2022-09-28 15:28:35 +0330

    add unit test project and write FizzBazz Rules

---
+++ Test/Test/FizzBazz.cs
@@ -0,0 +1,12 @@
+﻿namespace Test
+{
+    /// <summary>
+    /// check DividedBy3And5 then return FizzBazz
+    /// check DividedBy3 then return Fizz
+    /// check DividedBy5 then return Bazz
+    /// check NotDividedBy3or5 then return same number
+    /// </summary>
+    public class FizzBazz
+    {
+    }
+}
